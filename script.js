391a14316568fa1ffd57059eeb11181bb8ea925f
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2023-12-01 14:34:55 +0100

    add comments

---
+++ script.js
@@ -0,0 +1,33 @@
+/*
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto.
+
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sotto forma di stringhe
+*/
+
+
+
+// GENERARE un array di oggetti con le informazioni dei membri del team con nome, ruolo, foto
+const newArray = [
+
+    
+
+
+
+
+
+];
+
+
+
+// STAMPARE le informazioni in console
+
+
+
+// STAMPARE le informazioni sul DOM
